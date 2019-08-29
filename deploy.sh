b3e5df92360aa0288ebd4649b73fa3ed4743fe56
mahesadhanaa <mahesadhana91@gmail.com>
2019-08-29 20:00:36 +0700

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
+# deploy mysql and wordpress
+
 kubectl apply -f mysql-yaml/
 
 kubectl apply -f wp-yaml/
 
-sleep 60
-
-kubectl get svc wordpress
+# Will run forever...
+external_ip=""
+service="wordpress"
+while [ -z $external_ip ]; do
+ echo "Waiting for end point..."
+ external_ip=$(kubectl get svc $service --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+ [ -z "$external_ip" ] && sleep 10
+done
+echo 'End point ready:' && echo $external_ip
