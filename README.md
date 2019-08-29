a32003dc2e1adcef19dd125496fe74d3381e1a4b
mahesadhanaa <mahesadhana91@gmail.com>
2019-08-29 20:06:02 +0700

    update README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # wordpress-gke
+
+1. create cluster in GKE
+2. install google SDK
+3. run deploy.sh
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+# remote GKE
+
+gcloud container clusters get-credentials namecluster --zone asia-southeast1-a --project nameproject
+
 # deploy mysql and wordpress
 
 kubectl apply -f mysql-yaml/
